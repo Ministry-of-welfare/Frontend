558c42d21390c2030fc577681d29b242ba57fc96
rivkypaluch <rivkypaluch@gmail.com>
2025-09-01 16:58:23 +0300

    adding Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NewComponent } from './components/new/new.component';
 import { PersonComponent } from "./components/person/person.component";
 import { HttpClientModule } from '@angular/common/http';
-import { ClinicComponent } from "./components/clinic/clinic.component";
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewComponent, PersonComponent, HttpClientModule, ClinicComponent],
+  imports: [RouterOutlet,  PersonComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
