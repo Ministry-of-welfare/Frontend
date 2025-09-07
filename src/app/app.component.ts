93b526501e3cf6c68a0ac5e76eec4bb31bcab239
Esty <em0527104104@gmail.com>
2025-09-07 12:45:38 +0300

    create layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { EnvironmentsComponent } from "./components/environments/environments.component";
+import { LayoutComponent } from './layout/layout.component';
+
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,  HttpClientModule, EnvironmentsComponent],
+  imports: [RouterOutlet,  HttpClientModule, EnvironmentsComponent,LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/services/environments/environments.service.ts
+++ src/app/services/environments/environments.service.ts
@@ -7,12 +7,13 @@ import { Environment } from '../../models/environment.model';
   providedIn: 'root'
 })
 export class EnvironmentsService {
-  private BASE_URL = 'api/environments';
+  private BASE_URL = 'https://localhost:54525/api/Environments';
+
 
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<Environment[]> {
-    return this.http.get<Environment[]>(this.BASE_URL + '/getAll');
+    return this.http.get<Environment[]>(this.BASE_URL);
   }
 
   getById(id: number): Observable<Environment> {
