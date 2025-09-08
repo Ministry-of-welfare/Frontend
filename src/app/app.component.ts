63c9079d8115d36a3f6bf45866621f03747f8d6b
Esty <em0527104104@gmail.com>
2025-09-08 18:56:15 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,32 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { EnvironmentsComponent } from "./components/environments/environments.component";
 import { SearchFileComponent } from "./components/search-file/search-file.component";
 import { LayoutComponent } from './layout/layout.component';
-<<<<<<< Updated upstream
-
 import { FilesViewComponent } from './components/files-view/files-view.component';
-
-
-=======
-import { FilesViewComponent } from './components/files-view/files-view.component';
-
->>>>>>> Stashed changes
-
-
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< Updated upstream
-
-
-
-  imports: [RouterOutlet,  HttpClientModule,SearchFileComponent, EnvironmentsComponent,LayoutComponent,FilesViewComponent],
-
-
-=======
-  imports: [RouterOutlet,  HttpClientModule, EnvironmentsComponent,LayoutComponent,FilesViewComponent],
->>>>>>> Stashed changes
+  imports: [RouterOutlet, HttpClientModule, SearchFileComponent, EnvironmentsComponent, LayoutComponent, FilesViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
